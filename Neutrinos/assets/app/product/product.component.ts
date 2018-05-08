88fab5d72ff92cf3c01020319f344a12a4c69605
Prateek Sharma <uknowmm@users.noreply.github.com>
2018-05-08 02:57:36 -0400

    Update product.component.ts

--- Neutrinos/assets/app/product/product.component.ts
+++ Neutrinos/assets/app/product/product.component.ts
@@ -9,35 +9,34 @@ import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/compiler/src/core';
   templateUrl: './product.component.html'
 })
 export class ProductComponent {
- count:number=0;
-constructor(private pservice:ProductService){}
-products: Product[];
-cartProduct: Product[];
-
-
-ngOnInit(){
- this.pservice.getProduct().then((data) => { this.products = data
-  this.pservice.getCart().then((data) => { this.cartProduct = data;
-    this.cartProduct.forEach( cartpro=> {
-      this.products.forEach(product => {
-        if(product.productName === cartpro.productName){
-          product.button = true;
-        }else{
-          product.button = false;
+  count:number=0;
+  constructor(private pservice:ProductService){}
+  products: Product[];
+  cartProduct: Product[];
+  ngOnInit(){
+  this.loadValues();
+  }
+  loadValues(){
+    this.pservice.getProduct().then((data) => { this.products = data
+      this.pservice.getCart().then((data) => { this.cartProduct = data;
+        
+        for(let i = 0 ;i < this.products.length; i++){
+          for(let j = 0 ;j < this.cartProduct.length; j++){
+            if(this.products[i].productName === this.cartProduct[j].productName){
+              this.products[i].button = false;
+              break;
+            }else{
+              this.products[i].button = true;
+            }
+          }
         }
+    
+        });
       });
-    });
-
-    console.log(this.products)
-    });
-  });
- 
- 
-}
-
-onAdd(data:Product) {
-this.pservice.storeToCart(data);
-this.count+=1;
-  
-}
-}
+  }
+  onAdd(data:Product) {
+  this.pservice.storeToCart(data);
+  this.loadValues();
+  this.count+=1;
+  }
+  }

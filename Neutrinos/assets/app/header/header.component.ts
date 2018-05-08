95b3a5ca9b2bbdb31f674eb83dbf78699965e392
Prateek Sharma <uknowmm@users.noreply.github.com>
2018-05-07 22:17:27 -0400

    Update header.component.ts

--- Neutrinos/assets/app/header/header.component.ts
+++ Neutrinos/assets/app/header/header.component.ts
@@ -1,13 +1,13 @@
 import { Product } from './../product/prodect.model';
 import { ProductService } from './../product/product.service';
-import { Component } from '@angular/core';
+import { Component,Input } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
 export class HeaderComponent {
-
+  @Inupt('bind') itemcount;
   constructor(private productService:ProductService){
     
   }
